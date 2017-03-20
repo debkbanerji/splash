06ef5e4872bd1a84d777bec321129b77e3a026d3
deb <debkbanerji@gmail.com>
2017-03-20 19:12:44 -0400

    Extra Code For Sorting Quality Reports by Latitude

--- app/view-water-quality-reports/view-water-quality-reports.component.js
+++ app/view-water-quality-reports/view-water-quality-reports.component.js
@@ -7,5 +7,12 @@ angular.module('viewWaterQualityReports').component('viewWaterQualityReports', {
         self.viewWaterQualityReportsRef = firebase.database().ref().child("water-quality-reports");
         // self.sourceReports = $firebaseArray(self.viewWaterQualityReportsRef);
         self.qualityReportsArray = $firebaseArray(self.viewWaterQualityReportsRef);
+        self.qualityReportsArray.$loaded().then(function(x) {
+                console.log(x);
+                self.qualityReportsArray = x;
+            })
+            .catch(function(error) {
+                console.log("Error:", error);
+            });
     }]
 });
