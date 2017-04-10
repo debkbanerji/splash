615473cc547be7c97aca476477332a4579e9def5
Unknown <alizheng97@gmail.com>
2017-04-09 20:23:36 -0400

    Generating Graph
    Created input fields for generating a graph as well as a graph with dummy data

--- app/generate-graph/generate-graph.component.js
+++ app/generate-graph/generate-graph.component.js
@@ -4,6 +4,53 @@ angular.module('generateGraph').component('generateGraph', {
     controller: ['$routeParams', '$route', '$firebaseObject', '$firebaseArray', function generateGraphController($routeParams, $route, $firebaseObject, $firebaseArray) {
         var self = this;
         var user = firebase.auth().currentUser;
+        //var plotly = require('plotly')("AliceCZheng", "J9RukuNPkTxVefx0poBs");
         self.generateGraphRef = firebase.database().ref().child("users").child(user.uid).child("generate-graph");
+
+        google.charts.load('current', {packages: ['corechart', 'line']});
+        google.charts.setOnLoadCallback(drawBasic);
+
+        self.yearList = ["2015", "2016", "2017"];
+        self.virusOrContaminantList = ["Virus", "Contaminant"];
+        self.locationList = ["Atlanta", "Decatur"];
+
+
+        function drawBasic() {
+
+            var data = new google.visualization.DataTable();
+            data.addColumn('number', 'X');
+            data.addColumn('number', 'PPM');
+
+            data.addRows([
+                [0, 0],   [1, 10],  [2, 23],  [3, 17],  [4, 18],  [5, 9],
+                [6, 11],  [7, 27],  [8, 33],  [9, 40],  [10, 32], [11, 35],
+                [12, 30], [13, 40], [14, 42], [15, 47], [16, 44], [17, 48],
+                [18, 52], [19, 54], [20, 42], [21, 55], [22, 56], [23, 57],
+                [24, 60], [25, 50], [26, 52], [27, 51], [28, 49], [29, 53],
+                [30, 55], [31, 60], [32, 61], [33, 59], [34, 62], [35, 65],
+                [36, 62], [37, 58], [38, 55], [39, 61], [40, 64], [41, 65],
+                [42, 63], [43, 66], [44, 67], [45, 69], [46, 69], [47, 70],
+                [48, 72], [49, 68], [50, 66], [51, 65], [52, 67], [53, 70],
+                [54, 71], [55, 72], [56, 73], [57, 75], [58, 70], [59, 68],
+                [60, 64], [61, 60], [62, 65], [63, 67], [64, 68], [65, 69],
+                [66, 70], [67, 72], [68, 75], [69, 80]
+            ]);
+
+            var options = {
+                hAxis: {
+                    title: 'Week'
+                },
+                vAxis: {
+                    title: 'Contaminant/Virus PPM'
+                }
+            };
+
+            var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+
+            chart.draw(data, options);
+        }
     }]
+
+
+
 });
