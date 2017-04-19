db100ad2a60541958ef87991a034d0b99c15ce7d
Deb Banerji <debkbanerji@gmail.com>
2017-04-19 10:31:43 -0400

    Hiding Log Button If Necessary

--- app/nav-bar/nav-bar.component.js
+++ app/nav-bar/nav-bar.component.js
@@ -1,9 +1,22 @@
 angular.module('navBar').component('navBar', {
     templateUrl: 'nav-bar/nav-bar.template.html',
 
-    controller: ['$window', '$scope', '$location', function navBarController($window, $scope, $location) {
+    controller: ['$window', '$scope', '$location', '$firebaseObject', '$firebaseArray', function navBarController($window, $scope, $location, $firebaseObject, $firebaseArray) {
         var self = this;
         updateNavBar($location, self);
+        var user = firebase.auth().currentUser;
+        if (user) {
+            self.userRef = firebase.database().ref().child("registered-users").child(user.uid);
+            self.userObject = $firebaseObject(self.userRef);
+
+            self.userObject.$loaded(
+                function (x) {
+                    var userType = self.userObject["user-type"];
+                    hideElements(userType);
+                }, function (error) {
+                    console.error("Error:", error);
+                });
+        }
 
         $scope.$on('$routeChangeSuccess', function () {
             updateNavBar($location, self);
@@ -11,12 +24,12 @@ angular.module('navBar').component('navBar', {
 
         this.signOut = function () {
             console.log("BEFORE SIGNOUT:" + firebase.auth().currentUser.displayName);
-            firebase.auth().signOut().then(function() {
+            firebase.auth().signOut().then(function () {
                 // Sign-out successful.
                 console.log("SIGNED OUT");
                 $window.location.href = '/login';
                 $location.path('/login');
-            }, function(error) {
+            }, function (error) {
                 // An error happened.
             });
         }
@@ -38,4 +51,12 @@ function updateNavBar(location, self) {
     self.viewGraph = /view-graph$/.test(self.path);
     self.viewLog = /view-log$/.test(self.path);
     self.about = /about$/.test(self.path);
+}
+
+function hideElements(type) {
+    console.log(type);
+    if (type !== "ADMINISTRATOR") {
+        var viewLogButton = document.getElementById("viewLogButton");
+        viewLogButton.style.display = 'none';
+    }
 }
