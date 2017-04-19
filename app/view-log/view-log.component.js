73890d1ebc58ea46d2456d7e708ec394f9718a94
Deb Banerji <debkbanerji@gmail.com>
2017-04-19 09:50:35 -0400

    Added Logs

--- app/view-log/view-log.component.js
+++ app/view-log/view-log.component.js
@@ -4,6 +4,30 @@ angular.module('viewLog').component('viewLog', {
     controller: ['$routeParams', '$route', '$firebaseObject', '$firebaseArray', function viewLogController($routeParams, $route, $firebaseObject, $firebaseArray) {
         var self = this;
         var user = firebase.auth().currentUser;
-        self.viewLogRef = firebase.database().ref().child("users").child(user.uid).child("view-log");
+        self.viewLogRef = firebase.database().ref().child("security-log");
+        self.databaseLogs = $firebaseArray(self.viewLogRef);
+
+        self.logs = [];
+
+        self.databaseLogs.$loaded(
+            function(x) {
+                console.log(self.databaseLogs);
+                for (var i = 0; i < self.databaseLogs.length; i++) {
+                    item = self.databaseLogs[i];
+                    console.log(item);
+                    date = new Date(item["time"]);
+                    console.log(date.toUTCString());
+                    self.logs.push({
+                        "time" : date.toUTCString(),
+                        "type" : item["type"],
+                        "details" : item["details"],
+                        "uid" : item["user-uid"]
+                        }
+                    )
+                }
+                console.log(self.logs);
+            }, function(error) {
+                console.error("Error:", error);
+            });
     }]
 });
