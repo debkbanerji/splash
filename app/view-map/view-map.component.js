a995eacd0e475edb037b87d7356d84ed576cbd0a
Unknown <alizheng97@gmail.com>
2017-04-20 21:02:12 -0400

    Added Markers

--- app/view-map/view-map.component.js
+++ app/view-map/view-map.component.js
@@ -7,7 +7,32 @@ angular.module('viewMap').component('viewMap', {
         self.viewMapRef = firebase.database().ref().child("users").child(user.uid).child("view-map");
 
 
-        
+        // self.viewWaterSourceReportsRef = firebase.database().ref().child("water-source-reports");
+        // self.sourceReportsArray = $firebaseArray(self.viewWaterSourceReportsRef);
+        // var atlanta = {lat: 39.23, lng: -131.2};
 
+        // var marker = new google.maps.Marker({
+        //     position: atlanta,
+        //     map: map
+        // });
+
+
+        // self.sourceReportsArray.$loaded(
+        //     function(x) {
+        //         console.log(x);
+        //         for (var i = 0; i < x.length; i++) {
+        //             placeMarkerAndPanTo({lat: x[i].latitude, lng: x[i].longitude},map);
+        //         }
+        //     }, function(error) {
+        //         console.error("Error:", error);
+        //     });
+
+        // function placeMarkerAndPanTo(latLng, map) {
+        //     var marker = new google.maps.Marker({
+        //         position: latLng,
+        //         map: map
+        //     });
+        //     // map.panTo(latLng);
+        // }
     }]
 });
